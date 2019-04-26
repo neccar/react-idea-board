9d23c1dc6832275dcd2220f9f3638f93581bf711
Ahmet Neccaroglu <ahmetneccaroglu@gmail.com>
2019-04-26 18:07:21 +0100

    Updates

---
+++ precache-manifest.5b1c8f5177976f8282d9dbe8ab3894f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40bf97bd1fe467e275a4d00c22691d93",
+    "url": "/react-idea-board/index.html"
+  },
+  {
+    "revision": "5550e799cda450947ed4",
+    "url": "/react-idea-board/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "493a92503d922d7a4104",
+    "url": "/react-idea-board/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "5550e799cda450947ed4",
+    "url": "/react-idea-board/static/js/2.7bcec877.chunk.js"
+  },
+  {
+    "revision": "493a92503d922d7a4104",
+    "url": "/react-idea-board/static/js/main.5001143c.chunk.js"
+  },
+  {
+    "revision": "91c04882c6f63caec0e8",
+    "url": "/react-idea-board/static/js/runtime~main.f904e2e0.js"
+  }
+]);
